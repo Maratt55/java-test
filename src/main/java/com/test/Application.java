cad065c8a22d75b1638c9d0c4ccb689965aabea7
vahany <vahany@connectto.com>
2020-01-10 12:56:36 +0400

    first commit

--- src/main/java/com/test/Application.java
+++ src/main/java/com/test/Application.java
@@ -8,5 +8,6 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
         app.run(args);
+
     }
 }
